3a6d5b6a973aaaead119b6a420037ab9f2300789
TheCoderAdi-web <aadvik.kosuru@nesinternational.org>
2025-10-24 11:28:06 +0530

    Adding Enemies

--- game_data.py
+++ game_data.py
@@ -8,20 +8,40 @@ class Player():
         self.rect.x = pos[0]
         self.rect.y = pos[1]
 
-    def update(self):
+    def update(self, move_speed):
         dx: int
         dy: int
         dx, dy = 0, 0
 
         keys = pygame.key.get_pressed()
 
-        if keys[pygame.K_RIGHT]: dx += 1
-        if keys[pygame.K_LEFT]: dx -= 1
-        if keys[pygame.K_UP]: dy -= 1
-        if keys[pygame.K_DOWN]: dy += 1
+        if keys[pygame.K_RIGHT]: dx += move_speed
+        if keys[pygame.K_LEFT]: dx -= move_speed
+        if keys[pygame.K_UP]: dy -= move_speed
+        if keys[pygame.K_DOWN]: dy += move_speed
 
         self.rect.x += dx
         self.rect.y += dy
 
     def draw(self, screen):
         screen.blit(self.image, self.rect)
+
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self, pos):
+        super().__init__() 
+        
+        self.image: pygame.surface.Surface = pygame.surface.Surface((60, 60))
+        self.image.fill("blue")
+        
+        self.rect = self.image.get_rect()
+        self.rect.x = pos[0]
+        self.rect.y = pos[1]
+
+    def update(self):
+        # Enemy moves left by 1 pixel each frame
+        dx: int = -1 
+        self.rect.x += dx
+
+        # Check if the enemy is entirely off the left side of the screen
+        if self.rect.right < 0:
+            self.kill()
--- main.py
+++ main.py
@@ -1,19 +1,30 @@
 """Importing and Initializing Packages/Modules."""
 import pygame
 import sys
-from game_data import Player
+from random import randrange
+from game_data import Player, Enemy
 pygame.init()
 
 """Define all Global Variables."""
 RESOLUTION: tuple = (1000, 800)
+FPS = 60
 
 class Game():
     """Object to contain all of the game elements. Like the main operator class."""
-    def __init__(self, resolution):
-        self.screen = pygame.display.set_mode(resolution)
+    def __init__(self):
+        self.screen = pygame.display.set_mode(RESOLUTION)
         pygame.display.set_caption("Combat & Dodge")
 
+        self.clock = pygame.time.Clock()
+        
+        self.clock.tick(FPS)
+
         self.player = Player((30, 30), (515, 415), "red")
+
+        self.enemies = pygame.sprite.Group()
+
+        self.enemy_timer_max = 500
+        self.enemy_timer = self.enemy_timer_max
 
     def run(self):
         """Simply run the game, and include all drawing and update functions."""
@@ -25,8 +36,17 @@ class Game():
 
             self.screen.fill("black")
             
-            self.player.update()
+            self.player.update(0.5)
             self.player.draw(self.screen)
+
+            self.enemies.update()
+            self.enemies.draw(self.screen)
+
+            if self.enemy_timer > 0:
+                self.enemy_timer -= 1
+            else:
+                self.enemy_timer = self.enemy_timer_max
+                self.enemies.add(Enemy((RESOLUTION[0], randrange(0, RESOLUTION[1]))))
 
             pygame.display.flip()
 
@@ -36,5 +56,5 @@ class Game():
 
 """Run the game"""
 if __name__ == "__main__":
-    game: Game = Game(RESOLUTION)
+    game: Game = Game()
     game.run()
